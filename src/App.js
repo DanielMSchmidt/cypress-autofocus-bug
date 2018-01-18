7426f367dd88d350c775b725d78858d84ab3c678
Daniel Schmidt <dschmidt@mesosphere.com>
2018-01-18 12:42:39 +0100

    add auto select input field

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import logo from "./logo.svg";
+import "./App.css";
+
+class FieldAutofocus extends Component {
+  field = null;
+
+  componentDidMount() {
+    this.field.focus();
+  }
+
+  render() {
+    return <input ref={input => (this.field = input)} />;
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>vvv This should auto-focus vvv</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <FieldAutofocus />
       </div>
     );
   }
